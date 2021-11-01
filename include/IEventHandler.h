0185e56c06d6ab26ab7844d5ef31b22cca309fe6
David Parker <davidgparker123@utexas.edu>
2021-11-01 09:03:19 -0700

    fix comments

--- include/IEventHandler.h
+++ include/IEventHandler.h
@@ -4,7 +4,7 @@
 #include "InterruptController.h"
 #include "JoypadController.h"
 
-// Processes keyboard events to send to the joypad controller.
+// Processes input events to send to the joypad controller.
 class IEventHandler
 {
 public:
@@ -12,9 +12,9 @@ public:
     // Polls keyboard for input events, and sets the appropriate button presses on the joypad controller.
     virtual void HandleInput(JoypadController* joypadController) = 0;
 
-    // Should the emulator quit, writing any neccessary save state to disk?
+    // Should the emulator quit, writing any necessary save state to disk?
     virtual bool ShouldQuit() = 0;
 
-    // Indiciates what speed multiplier the emulator should actively run at.
+    // Indicates what speed multiplier the emulator should actively run at.
     virtual int SpeedMultiplier() = 0;
 };
